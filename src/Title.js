4a7d682ccdcc52cdb5c63376226269e4249bfb55
Ansh Vijay <anshvijay28@gmail.com>
2023-03-07 16:20:36 -0500

    title component

---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import './Title.css'
+
+export default function Title() {
+    return (
+        <div className = "title">
+            <h1 className = "title-text">ChatGPT Clone</h1>
+        </div>
+    );
+}
