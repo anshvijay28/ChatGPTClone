48947c0f5b530e9c68a7901bb36df73864f07e15
Ansh Vijay <anshvijay28@gmail.com>
2023-03-15 22:14:08 -0400

    added conversation element and lifted state to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Title from './Title.js';
+import Input from './Input.js';
+import Conversation from './Conversation.js';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [prompts, setPrompts] = useState([]);
+
   return (
     <div className="App">
       <Title />
+      <Conversation prompts = {prompts}/>
+      <Input prompts = {prompts} setPrompts = {setPrompts}/>
     </div>
   );
 }
